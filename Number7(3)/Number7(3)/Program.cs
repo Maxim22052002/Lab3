47d731806831d72fbd2b8768d6deaa2f3c3214eb
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 20:34:35 +0300

    T7

---
+++ Number7(3)/Number7(3)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Number7_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the element of the fibonacci series: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(N+" element of the fibonacci series: ");
+            Console.WriteLine(FibonacciRecursive(N));
+        }
+        static long FibonacciRecursive(int n)
+        {
+            
+
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
+    }
+}
